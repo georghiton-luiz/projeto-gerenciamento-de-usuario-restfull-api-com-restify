a44f4e69809eebbb682914e772ec61792d8cf7a3
georghiton-luiz <georghiton@hotmail.com>
2021-07-09 12:08:33 -0300

    Refatorando para uma classe HttpRequest

--- client-server/public/controllers/UserControllers.js
+++ client-server/public/controllers/UserControllers.js
@@ -166,30 +166,17 @@ class UserControllers{
 
         //let users = User.getUserStorege();
 
-        let ajax = new XMLHttpRequest();
+        HttpRequest.get("/users").then(data => {
 
-        ajax.open("GET", "/users");
-
-        ajax.onload = event => {
-
-            let obj = {users:[]};
-
-            try{
-                obj = JSON.parse(ajax.responseText);
-            }catch(e){
-                console.error(e);
-            }
-
-            obj.users.forEach(dataUser => {
+            data.users.forEach(dataUser => {
 
                 let user = new User();
     
                 user.loadFromJSON(dataUser);
     
                 this.addLine(user);
-            });               
-        };
-        ajax.send();
+            }); 
+        });     
     }
 
     addLine(dataUser){
